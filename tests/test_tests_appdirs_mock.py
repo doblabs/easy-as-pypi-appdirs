48a1d60d8cf392ff54ae1ba7c4f78478f06d1aa7
landonb <landonb@users.noreply.github.com>
2022-10-04 00:58:37 -0500

    WIP

--- tests/test_tests_appdirs_mock.py
+++ tests/test_tests_appdirs_mock.py
@@ -37,12 +37,12 @@ class TestTestsAppdirsMock():
     def register_application(self, app_name):
         register_application(app_name)
 
-    def test_tests_xdg_appdirs_mock_safe_effect_EARLY(self, xdg_appdirs):
-        adir_path = xdg_appdirs.safe.user_cache_dir
-# macos: adir_path: /Users/runner/Library/Caches/easy-as-pypi-appdirs-tests
-        print(f"adir_path: {adir_path}")
-        #assert False
-        assert not os.path.exists(adir_path)
+#      def test_tests_xdg_appdirs_mock_safe_effect_EARLY(self, xdg_appdirs):
+#          adir_path = xdg_appdirs.safe.user_cache_dir
+#  # macos: adir_path: /Users/runner/Library/Caches/easy-as-pypi-appdirs-tests
+#          print(f"adir_path: {adir_path}")
+#          #assert False
+#          assert not os.path.exists(adir_path)
 
     def test_tests_tmp_appdirs_mock_side_effect(self, tmp_appdirs):
         adir_path = tmp_appdirs.user_cache_dir
@@ -54,10 +54,24 @@ class TestTestsAppdirsMock():
         print(f"adir_path: {adir_path}")
         assert not os.path.exists(adir_path)
 
-    #  def test_tests_xdg_appdirs_mock_side_effect(self, xdg_appdirs):
-    #      adir_path = xdg_appdirs.user_cache_dir
-    #      print(f"adir_path: {adir_path}")
-    #      assert os.path.exists(adir_path)
+# FIXME/2022-10-04 00:52: macos: Not removing the directory...
+    def test_tests_xdg_appdirs_mock_side_effect(self, xdg_appdirs):
+        assert not os.path.exists(adir_path)
+
+        adir_path = xdg_appdirs.user_cache_dir
+
+#          print(f"adir_path: {adir_path}")
+#          adir_path = xdg_appdirs.safe.user_cache_dir
+#  # macos: adir_path: /Users/runner/Library/Caches/easy-as-pypi-appdirs-tests
+#          print(f"adir_path: {adir_path}")
+#          assert False
+
+        assert os.path.exists(adir_path)
+        # Not sure why, but next text fails in macOS (via GitHub actions)
+        # because directory exists. So remove the directory. This os.rmdir
+        # is not necessary when I test locally on Ubuntu (I have not tried
+        # locally on macOS).
+        os.rmdir(adir_path)
 
     def test_tests_xdg_appdirs_mock_safe_effect(self, xdg_appdirs):
         adir_path = xdg_appdirs.safe.user_cache_dir
