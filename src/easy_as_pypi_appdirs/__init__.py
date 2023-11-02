64a58e60cdb0b79d7b4561877715be12f44be1cc
Landon Bouma <landonb@users.noreply.github.com>
2023-11-02 01:48:25 -0500

    Build: Add package __version__ member

--- src/easy_as_pypi_appdirs/__init__.py
+++ src/easy_as_pypi_appdirs/__init__.py
@@ -35,3 +35,13 @@ from .exists_or_mkdirs import (  # noqa: F401
     must_ensure_file_path_dirred,
 )
 from .expand_and_mkdirs import must_ensure_appdirs_path  # noqa: F401
+
+# This version value is substituted on poetry-build. See pyproject.toml:
+#   [tool.poetry-dynamic-versioning.substitution]
+# - So when installed in 'editable' mode, the substitution does not happen,
+#   and __version__ remains "".
+#   - But we only use __version__ for .github/workflows/release-smoke-test.yml
+#     and not for anything else (otherwise we could check Git tags when
+#     __version__ == "", if we assume an 'editable' mode install only happens
+#     on a dev machine).
+__version__ = ""
